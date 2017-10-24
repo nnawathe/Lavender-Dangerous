7450b5098d731053d1da050eb45c808a647d3ebb
jakecelentano <jcelentano@umass.edu>
2017-10-24 13:13:31 -0400

    added product admin class

--- project1/LavenderDangerous/catalog/admin.py
+++ project1/LavenderDangerous/catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, Category, User, ShoppingCart, Review, Requests
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'id', 'quantity', 'description', 'image')
+    fields = ['name', 'price', 'id', 'quantity', 'description', 'image']
+
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ()
