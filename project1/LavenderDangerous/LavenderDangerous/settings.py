c02a2b913eaba6d936ce375bdff32add2d3c01b0
Nishant Nawathe <nnawathe@umass.edu>
2017-10-24 13:14:18 -0400

    created compsci326 password

--- project1/LavenderDangerous/LavenderDangerous/settings.py
+++ project1/LavenderDangerous/LavenderDangerous/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6d97=agm5&0r3nnk(w^$ykb8_#zeqvq-$w*vd3)16xop$i+1al'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
