f76b4608edbc537a5f0f0339e1e547c8a4eb3e81
williamwarner <willyak@gmail.com>
2017-10-31 13:32:29 -0400

    Template views.py

--- project1/LavenderDangerous/catalog/views.py
+++ project1/LavenderDangerous/catalog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Product, User, ShoppingCart, Request, Review, Category
+
+def index(request):
+    pass
+    
+def product(request):
+    pass
+    
+def user(request):
+    pass
+    
+def requests(request):
+    pass
+    
+def faq(request):
+    pass
