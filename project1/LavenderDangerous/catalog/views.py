36bea429450dd3d15d701acf6f4861f33f363339
Willinder <sevrinwillinder@gmail.com>
2017-11-02 11:26:15 -0400

    some cart total math is working

--- project1/LavenderDangerous/catalog/views.py
+++ project1/LavenderDangerous/catalog/views.py
@@ -40,11 +40,15 @@ def cart(cart):
     item2=ShoppingCart.objects.first().product.all()[1]
     price1=ShoppingCart.objects.first().product.first().display_price()
     price2=ShoppingCart.objects.first().product.all()[1].display_price()
+    subtotal=price1+price2
+    shipping=3.99
+    total=shipping+subtotal
 
     return render(
         cart,
         'cart.html',
-        context={'item1': item1, 'item2': item2, 'price1': price1, 'price2': price2}
+        context={'item1': item1, 'item2': item2, 'price1': price1, 'price2': price2, 'subtotal': subtotal,
+        'total': total, 'shipping': shipping}
     )
 
 def user(user):
