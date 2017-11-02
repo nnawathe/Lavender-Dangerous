7168f439ce4db1950b860fa7d5ca353383770570
Nishant Nawathe <nnawathe@umass.edu>
2017-11-02 02:19:40 -0400

    fixed requests bug

--- project1/LavenderDangerous/catalog/views.py
+++ project1/LavenderDangerous/catalog/views.py
@@ -48,8 +48,8 @@ def user(user):
 
 def requests(request):
     num_requests=Request.objects.count()
-    item1=Request.objects.filter(popularity=1).first()
-    user1=Request.objects.filter(popularity=1).first().display_user
+    item1=Request.objects.filter(popularity=2).first()
+    user1=Request.objects.filter(popularity=2).first().display_user
     item2=Request.objects.filter(popularity=0).first()
     user2=Request.objects.filter(popularity=0).first().display_user
 
